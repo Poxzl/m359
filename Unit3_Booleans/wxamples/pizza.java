697bf5112566d8fabcc2c6a07a9dd3982e42c50f
ArjunC <84456473+Poxzl@users.noreply.github.com>
2021-09-15 09:57:02 -0500

    dgssxaxasxasaa
    gerew

--- Unit3_Booleans/wxamples/pizza.java
+++ Unit3_Booleans/wxamples/pizza.java
@@ -37,8 +37,7 @@ public class pizza {
 
     //create an output with the pizzas onfo, but instead of printing it, return it
     public String toString(){
-        String output = this.inches + "inch pizzas from blnfhwofhwhgp" +
-                 + this.store;
+        String output = this.inches + "inch pizzas from jo " + this.store;
         output += "toppings: " + this.numToppings;
         output += "has deep dish? " + this.isDeepDish;
         return output;
