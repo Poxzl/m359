36eebdb81ac759a84fa3495bebf58dad0ecacf21
ArjunC <84456473+Poxzl@users.noreply.github.com>
2021-09-16 09:32:45 -0500

    update
    dfad

--- Unit3_Booleans/homework/Review.java
+++ Unit3_Booleans/homework/Review.java
@@ -1,5 +1,25 @@
 package Unit3_Booleans.homework;
 
 public class Review {
-    
+    private String reviewer;
+    private String buisness;
+    private int rating;
+
+    public Review(String reviewer, String buisness, int rating) {
+        this.reviewer = reviewer;
+        this.buisness = buisness;
+        this.rating = rating;
+    }
+
+    public String toString(){
+        String output = reviewer + ", " + buisness + ", " + rating;
+
+        if (rating == 5){
+            output += "/nHIGLY GOOD";
+        }
+        else if (rating == 1){
+            output += "/nHIGLY BAD";
+        }
+        return output;
+    }
 }
