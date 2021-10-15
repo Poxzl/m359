e54bf2be1305c7e7fbfa95c282a1bd07619fe770
ArjunC <84456473+Poxzl@users.noreply.github.com>
2021-10-15 10:09:13 -0500

    erf
    efe

--- Unit4/LabPilgsitalatin/PigLatin.java
+++ Unit4/LabPilgsitalatin/PigLatin.java
@@ -1,5 +1,15 @@
 package Unit4.LabPilgsitalatin;
 
 public class PigLatin {
-    public static String 
+    public static String toPigLatin(String str){
+
+    }
+    public static String translateWordToPigLatin(String word){
+        boolean strtVow;
+        String letter = word.substring(0, 1);
+        if(letter.equalsIgnoreCase("a") || letter.equalsIgnoreCase("e") || letter.equalsIgnoreCase("i") || letter.equalsIgnoreCase("o") || letter.equalsIgnoreCase("u")){
+            strtVow = true;
+        }
+        
+    }
 }
