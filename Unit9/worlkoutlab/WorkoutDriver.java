6404369067945e998b7bea65a4525ae28e547504
ArjunC <84456473+Poxzl@users.noreply.github.com>
2022-03-11 10:08:30 -0600

    kill me
    asdfasd

--- Unit9/worlkoutlab/WorkoutDriver.java
+++ Unit9/worlkoutlab/WorkoutDriver.java
@@ -1,4 +1,7 @@
 package Unit9.worlkoutlab;
 
 public class WorkoutDriver {
+    public static void main(String[] args) {
+        WorkoutPlan lol = new WorkoutPlan(4);
+    }
 }
--- Unit9/worlkoutlab/WorkoutPlan.java
+++ Unit9/worlkoutlab/WorkoutPlan.java
@@ -8,10 +8,32 @@ import java.util.ArrayList;
 public class WorkoutPlan {
     private int workoutsCompleted;
     private int workoutsSkipped;
-    private int curIdxPlan;
+    private int curWeek;
     private int nxtWorkoutNumber;
     private int calsBurned;
     private int minsExercise;
-    private Workout[][] workouts = new Workout[4][7];
+    private Workout[][] workouts;
 
+    public WorkoutPlan(int numWeeks){
+        fillWeeks(numWeeks);
+    }
+
+    private void fillWeeks(int numWeeks){
+        workouts = new Workout[numWeeks][7];
+        for (int i = 0; i < numWeeks; i++) {
+            for (int j = 0; j < 7; j++) {
+                int choice = (int) Math.random() * 3;
+                if(choice == 1){
+                    workouts[i][j] = new Cardio("Running", 0, (int)Math.random() * 30 + 10, 0, Math.random() * 3 + 1);
+                }
+                if(choice == 2){
+                    workouts[i][j] = new Strength("Lifting", 0, (int)Math.random() * 46 + 15, (int)Math.random() * 140 + 95);
+                }
+                if(choice == 3){
+                    workouts[i][j] = new Wellness("Stretching", 0, (int)Math.random() * 31 + 30, (int)Math.random() * 5 + 8);
+                }
+                System.out.println(workouts[i][j]);
+            }
+        }
+    }
 }
